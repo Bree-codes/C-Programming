597c793fdffd5f875960e6c86f82585d892c4862
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-08-02 08:08:44 +0300

    Simple example to illustrate pass by reference

--- PassbyReference/main.c
+++ PassbyReference/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    printf("Hello Bree!\n");
+    int a = 5,b = 10,c = 7;
+
+    printf("Enter 3 numbers: ");
+    scanf("%d %d %d" ,&a,&b,&c);
+    int result = a + b+ c;
+    printf("The result is: %d\n",result);
     return 0;
 }
