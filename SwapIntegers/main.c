7cc995a50d690c192b271f2fd5106151bd73ebeb
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-08-02 08:42:54 +0300

    Pass by reference using pointers

--- SwapIntegers/main.c
+++ SwapIntegers/main.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
+
+//function for swapping 2 integers
 void swap(int *a,int *b){
 
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
+
+//main method
 int main(void) {
      int x = 17,y = 10;
 
+    printf("Before swapping: x = %d and y = %d\n",x,y);
+
+    //calling swap method
+    swap(&x,&y);
+
+    printf("After swapping: x = %d and y = %d\n",x,y);
 
     return 0;
 }
