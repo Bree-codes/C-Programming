90a54c669da4ed3f9f573c945f3152a35a3cbe1a
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-08-04 12:17:54 +0300

    Opening and reading a file using system calls

--- SystemCalls/OpenAndReadFile/main.c
+++ SystemCalls/OpenAndReadFile/main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+#define BUF_SIZE 1024
+
+int main(int argc,char* argv[]) {
+    int fd = open(argv[1],O_RDONLY);
+    char buf[BUF_SIZE];
+
+    if(fd != -1) {
+        ssize_t fileRead = read(fd, buf, BUF_SIZE - 1);
+
+        while (fileRead > 0) {
+            buf[fileRead] = '0';
+            printf("%s\n", buf);
+            fileRead = read(fd, buf, BUF_SIZE - 1);
+        }
+    }
+        else{
+            printf("Error opening file\n");
+            return -1;
+        }
+
     return 0;
 }
