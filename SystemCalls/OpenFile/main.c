3d411422d4c1e62e9fa6f75d09dfb4baaf797b0f
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-08-04 11:46:38 +0300

    Fork() method in system calls

--- SystemCalls/OpenFile/main.c
+++ SystemCalls/OpenFile/main.c
@@ -1,6 +1,13 @@
+#include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+int main(int argc, char* argv[]){
+    int id = fork();
+    printf("Hello from ID: %d\n",id);
+    printf("Current Process ID: %d\n",getpid());
+    printf("Parent Process ID: %d\n",getppid());
+
     return 0;
 }
