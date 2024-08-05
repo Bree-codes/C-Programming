90c12c481334684b07a1a9731992f200082c6656
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-08-05 11:26:37 +0300

    Easier way to write to file

--- SystemCalls/WriteToFile/main.c
+++ SystemCalls/WriteToFile/main.c
@@ -3,16 +3,39 @@
 #include <unistd.h>
 
 int main(void) {
-    int fd = open("file.txt", O_WRONLY| O_CREAT, S_IRUSR | S_IWUSR | S_IXUSR);
+ /*   int fd = open("file.txt", O_WRONLY| O_CREAT, S_IRUSR | S_IWUSR | S_IXUSR);
     if (fd > -1){
-        write(fd,"Hello all and welcome to my class.\n"
-                 "Today I'll be taking you through system calls in C Programming!\0",98);
+        ssize_t bytesWritten = write(fd, "Hello all and welcome to my class.\n"
+                                         "Today I'll be taking you through system calls in C Programming!\0\n", 98);
+        if (bytesWritten == -1) {
+            perror("write");
+            close(fd);
+            return 1;
+        }
         close(fd);
-    }
-    else{
-        printf("Error opening or creating file!");
+        printf("Message was successfully written to your 'file.txt' file\nOpen it to confirm.\n");
+    } else {
+        perror("open");
+        return 1;
     }
 
-    printf("Message was successfully written to your 'file.txt' file\nOpen it to confirm..");
+    return 0;*/
+
+
+    int fd = open("output.txt",O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+    if(fd > -1){
+        ssize_t  bytesWritten = write(fd,"Hello, World!\0\n",13);
+        if(bytesWritten == -1){
+            perror("write");
+            close(fd);
+            return 1;
+        }
+        close(fd);
+        printf("Message written successfully.\n");
+    }
+    else{
+        perror("open");
+        return 1;
+    }
     return 0;
 }
