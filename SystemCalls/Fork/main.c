9bdbce4c6bf559413928f4594bfacc5bbef9a33f
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-08-08 10:52:13 +0300

    Fork system call

--- SystemCalls/Fork/main.c
+++ SystemCalls/Fork/main.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <unistd.h> // For fork()
 
 int main() {
-    pid_t pid = fork();
+    pid_t pid = fork(); // Create a new process
 
-    if (pid == 0) {
-    // Child process
-    printf("This is the child process.\n");
-    }
-    else {
-    // Parent process
-    printf("This is the parent process.\n");
+    if (pid < 0) {
+        // Fork failed
+        perror("fork failed");
+        return 1;
+    } else if (pid == 0) {
+        // This block is executed by the child process
+        printf("I am the child process.My PID is %d.\n", getpid());
+    } else {
+        // This block is executed by the parent process
+        printf("I am the parent process.My PID is %d and my child's PID is %d.\n", getpid(), pid);
     }
 
     return 0;
-    }
+}
