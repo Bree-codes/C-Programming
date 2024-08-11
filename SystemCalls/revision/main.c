fe62f1ec0bd0230159a28c66ce3e4a695d2327dc
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-08-11 21:13:42 +0300

    revision

--- SystemCalls/revision/main.c
+++ SystemCalls/revision/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <malloc.h>
 
 int main(void) {
-    mode_t old_umask;
+ /*   mode_t old_umask;
 
     // Step 1: Set umask to 0 and capture the old umask
     old_umask = umask(0);
@@ -12,7 +13,38 @@ int main(void) {
     umask(old_umask);
 
     // Now you have the old umask value without changing it
-    printf("Current umask is: %04o\n", old_umask);
+    printf("Current umask is: %04o\n", old_umask);*/
+
+    //malloc and realloc functions
+    int *arr = (int*)malloc(5 * sizeof(int));
+
+    //checking if memory allocation using malloc was successful
+    if(arr == NULL){
+        perror("Memory Allocation Failed");
+        return 1;
+    }
+    //initialize and print values to the array
+    for(int i = 0;i < 5;i++){
+        arr[i] = i +1;
+        printf("%d",arr[i]);
+    }
+    printf("\n");
+
+    //Reallocating the memory using reallo()
+    arr = (int*) realloc(arr,10* sizeof(int));
+    if(arr == NULL){
+        perror("Memory Reallocation Failed");
+        return 1;
+    }
+    for(int i = 5;i < 10;i++){
+        arr[i] = i+1;
+    }
+    for(int i = 0;i < 10;i++){
+        printf("%d",arr[i]);
+    }
+    printf("\n");
+
+    free(arr);
 
     return 0;
 }
