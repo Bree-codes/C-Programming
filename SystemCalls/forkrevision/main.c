a29df3605cf03c3401a18b05545677a9d9585a6d
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-08-12 07:09:56 +0300

    Revision

--- SystemCalls/forkrevision/main.c
+++ SystemCalls/forkrevision/main.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    pid_t pid = fork();
+
+    if(pid < 0){
+        perror("Fork Failed:");
+        return 1;
+    }
+    else if(pid == 0){
+        //child process
+        printf("This is the child process:\n");
+        printf("Process Id is:%d\n",getpid());
+        printf("The parent process Id is:%d\n",getppid());
+        printf("\n");
+    }
+    else{
+        //parent process
+        printf("This is the parent process:\n");
+        printf("Process Id is:%d\n",getpid());
+        printf("Child process Id:%d\n",pid);
+        printf("\n");
+
+        wait(NULL);
+    }
+
     return 0;
 }
