fd62983a3f0561cf4fc2ae209b23e0422045e028
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-08-12 07:48:35 +0300

    Revision

--- SystemCalls/forkrevision/main.c
+++ SystemCalls/forkrevision/main.c
@@ -2,8 +2,12 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 
 int main(void) {
+/*
+
+    //forking
     pid_t pid = fork();
 
     if(pid < 0){
@@ -26,6 +30,16 @@ int main(void) {
 
         wait(NULL);
     }
+*/
+
+    //this call sets the mask to 0 and returns the old mask before changing it to 0.
+    mode_t old_umask = umask(0);
+    printf("The original umask was: %04o\n", old_umask);
+    printf("The umask is now set to 0: %04o\n", 0);
+
+    //we reset the umask to the original mask before it was changed to 0.
+    umask(old_umask);
+    printf("The current mask is: %04o\n",old_umask);
 
     return 0;
 }
